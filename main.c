cc3d7fcb134bfdb0a7c76d09ad3d8756e88e3882
Oladipo <phaymos2022@gmail.com>
2023-04-26 02:43:44 -0700

    auhors

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - does the shell main work
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+printf("$\n");
+}
