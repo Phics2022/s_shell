e0e84e2b27983bc0e9d981b61d6caface7b0eb45
oladipo <phaymos2022@gmail.com>
2023-05-13 12:57:17 +0100

    task"

---
+++ getline.c
@@ -0,0 +1,2 @@
+#include "phics.h"
+
---
+++ phics.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <signal.h>
