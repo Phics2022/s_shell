099121fa6d316842534859f6b1bdc80246354940
oladipo <phaymos2022@gmail.com>
2023-05-13 17:42:31 +0100

    task

--- cprp.c
+++ cprp.c
@@ -2,21 +2,23 @@
 int main(void)
 {
 	char *path =  getenv("PATH");
+	char *args[4000];
 	char *tok = strtok(path, ":");
 	int fd;
 	int i = 0;
-	char *args[4000];
 	while(tok != NULL)
 	{
-		fd = open("path", O_CREAT | O_WRONLY | O_TRUNC);
+		fd = open("path", O_RDWR | O_CREAT | O_TRUNC);
 		write(fd, tok, strlen(tok));
 		write(fd, "/ls", 3);
-		read(fd, args[i], strlen(tok) + 3);
+		args[i] = malloc(strlen(tok) + 3);
+		read(fd, args[i], (strlen(tok) + 3));
 		close(fd);
 		printf("%s\n", args[i]);
 		i++;
 		tok = strtok(NULL, ":");
 	}
 	args[i] = NULL;
-
+	printf("%s", args[0]);
+return (0);
 }
