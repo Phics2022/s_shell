c41fb172edfb9ad6f45fd87556621f19d86a8d13
oladipo <phaymos2022@gmail.com>
2023-05-13 23:37:30 +0100

    task

--- cprp.c
+++ cprp.c
@@ -5,18 +5,21 @@ int main(void)
 	char *tok = strtok(path, ":");
 	int fd;
 	int i = 0;
-	char *args[4000];
+	char **args = (char **)malloc(sizeof(char *) *4000);
 	while(tok != NULL)
 	{
-		fd = open("path", O_CREAT | O_WRONLY | O_TRUNC);
+		fd = open("path.txt", O_RDWR);
 		write(fd, tok, strlen(tok));
 		write(fd, "/ls", 3);
-		read(fd, args[i], strlen(tok) + 3);
+		char reader[1024];
+		read(fd, reader,strlen(tok) + 3);
+		printf("%s\n", reader);
+		//*(args + 1) = reader;
 		close(fd);
-		printf("%s\n", args[i]);
+		//printf("%s\n", *(args + i));
 		i++;
 		tok = strtok(NULL, ":");
 	}
-	args[i] = NULL;
+	*(args + i) = NULL;
 
 }
