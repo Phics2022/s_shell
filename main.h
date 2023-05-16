d5fd3dc7e9bfcdb57bade67c6e05a54cfee09f5a
oladipo <phaymos2022@gmail.com>
2023-05-16 16:46:40 +0100

    task

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "phics.h"
+extern char **environ;
 ssize_t _getline(char **ptr, size_t *n, FILE *stream);
 char *_strcat(char *s1, char *s2);
 void _strtok(char *command, char *arg[]);
---
+++ setenv.c
@@ -0,0 +1,46 @@
+#include "phics.h"
+#include "main.h"
+int _setenv( char *name, char *value, int overwrite)
+{
+	int env_length, total_length, i;
+	char **new_env;
+	char *new_var;
+	/*check if val exists and if overwite is allowed*/
+	/*by default i made overwrite un available by giving it the value of 0*/
+	if(getenv(name) == 0 && overwrite == 0)
+		return (0);
+
+	/*if not*/
+	/*get the lenght of environ*/
+	for (env_length = 0; environ[env_length] != NULL; env_length++);
+	/*allocate memory for the new environ*/
+	*new_env = malloc(sizeof(char *) * (env_length + 2));
+	/*copy environ into new environ*/
+	for(i = 0; i < env_length; i++)
+	{
+		new_env[i] = environ[i];
+		/*note i did not null terminate after the loop*/
+	}
+	/*copy the name=value into the new var*/
+	total_length = strlen(name) + strlen(value) + 2;
+	new_var = malloc(sizeof(char) * total_length);
+	strcat(new_var, name);
+	strcat(new_var, "=");
+	strcat(new_var, value);
+	/* copy new var into end of new environ*/
+	new_env[env_length] = new_var;
+	new_env[env_length + 1] = NULL;
+	/* change environ to new env*/
+	environ = new_env;
+	free(new_env);
+	free(new_var);
+	return (0);
+}
+
+int main(void)
+{
+	_setenv("PHICS", "SE", 0);
+	setenv("CHURA", "SE", 0);
+	printf("success\n");
+	return 0;
+}
